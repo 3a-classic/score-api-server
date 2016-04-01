7cc3e640e2aad90f20cc31fd4ea940d44298ccfd
circle user <circle@3a-classic1.localdomain>
2016-04-01 07:32:24 +0000

    add 20160402

---
+++ util/20160402/3a-classic_register.sh
@@ -0,0 +1,8 @@
+# userRegister
+curl -X POST -H "Content-Type : application/json"  -d @3a-classic_user.json api.3a-classic.com/v1/register/user
+
+## teamRegister
+curl -X POST -H "Content-Type : application/json"  -d @3a-classic_team.json api.3a-classic.com/v1/register/20160402/team
+
+## fieldRegister
+curl -X POST -H "Content-Type : application/json"  -d @3a-classic_field.json api.3a-classic.com/v1/register/20160402/field
